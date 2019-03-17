84f6636c45471931e92667ce79d10564cc4f4ea8
Aizhan Tilek kyzy <aijanm01@gmail.com>
2019-03-17 21:58:29 +0600

    fix game

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="checkin">    
+    <Checkin />
+  </div>
+</template>
+
+<script>
+
+import Checkin from '@/components/Checkin.vue'
+
+export default {
+  name: 'checkin',
+  components: {
+   Checkin
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="monsterSlayer">    
+    <MonsterSlayer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MonsterSlayer from '@/components/MonsterSlayer.vue'
+
+export default {
+  name: 'home',
+  components: {
+   MonsterSlayer
+  }
+}
+</script>
